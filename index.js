4d59a6cbfaeae79fe31214d518f330f9588f13ce
PiyushKhandelia <piyushkhandelia2002@gmail.com>
2024-09-18 18:37:32 +0530

    Initial commit 3

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://piyushkhandelia.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Load intents JSON
 let intents;
